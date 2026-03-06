36c23b14a9a3b7d0a1a8e8534f7846b76167502d
Tiago Mota <109996403+TiagoSM0203@users.noreply.github.com>
2026-03-06 08:49:20 -0300

    start

---
+++ braschlor/src/styles.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalCss = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "Outfit", sans-serif;
+    }
+`
+
+const Cores = {
+    white: "#FFFFFF",
+    green: "#8CC64B",
+    black: "#000000",
+    gray: "#787878",
+    blueLight: "#0082DB",
+    blueDark: "#004575",
+    grayLight: "#d9d9d9"
+}
